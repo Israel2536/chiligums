27bec644d61976c4f5f586efa44380d208bc55fa
Alejandro Portilla <portillaisrael9@gmail.com>
2025-10-05 14:45:23 -0500

    fix: vite config base path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/chiligums/',  // 👈 nombre exacto del repo
-});
+    base: '/chiligums/', // 👈 nombre exacto del repo
+})
